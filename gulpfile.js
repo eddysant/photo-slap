bf555c4f97d4bcf3797dd3e06aa8779d4a45ce19
Eddy <eddysant@users.noreply.github.com>
2016-06-07 13:51:11 -0400

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+ 
+var gulp = require('gulp'),
+	eslint = require('gulp-eslint'),
+	eslintThreshold = require('gulp-eslint-threshold');
+ 
+gulp.task('eslint', function() {
+	var thresholdWarnings = 3;
+ 
+	return gulp.src(['**/*.js', '!node_modules/**'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslintThreshold.afterWarnings(thresholdWarnings, function (numberOfWarnings) {
+			throw new Error('ESLint warnings (' + numberOfWarnings + ') equal to or greater than the threshold (' + thresholdWarnings + ')');
+		}));
+});
+ 
+gulp.task('default', ['eslint']);
