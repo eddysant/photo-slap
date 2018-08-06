2c9276f1e56f4151723a0e72abc72585ef14984c
eddy sant <eddysant@gmail.com>
2018-08-06 16:28:33 -0400

    Updated packages and removed no longer supported git tools from readme

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ gulp.task('eslint', function() {
   const thresholdWarnings = 3;
  
   return gulp.src(['**/*.js', '!node_modules/**'])
-  .pipe(eslint())
-	.pipe(eslint.format())
-	.pipe(eslintThreshold.afterWarnings(thresholdWarnings, (numberOfWarnings) => {
-  throw new Error('ESLint warnings (' + numberOfWarnings + ') equal to or greater than the threshold (' + thresholdWarnings + ')');
-	}));
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslintThreshold.afterWarnings(thresholdWarnings, (numberOfWarnings) => {
+      throw new Error('ESLint warnings (' + numberOfWarnings + ') equal to or greater than the threshold (' + thresholdWarnings + ')');
+    }));
 		
 });
  
